c7f042d9f563322508ac42b4843b871b9061bd97
Miguel <miguelbermell@gmail.com>
2022-05-09 19:44:06 +0200

    Add dev environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appName: 'News App',
+  projectUrl: 'http://localhost:4200',
+  apiUrl: 'http://127.0.0.1',
 };
 
 /*

556310d8c8081d06b8da8f5b4439c98db1931ce2
Miguel <miguelbermell@gmail.com>
2022-05-12 00:36:05 +0200

    Add hot-toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule, LOCALE_ID } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router'
+import { AppRoutingModule } from './app-routing.module'
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { UiModule } from './shared/ui/ui.module'
+import es from '@angular/common/locales/es'
+import { registerLocaleData } from '@angular/common'
+import { HotToastModule } from '@ngneat/hot-toast'
 
-import { AppComponent } from './app.component';
-import { PagesModule } from './core/pages/pages.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+registerLocaleData(es)
 
 @NgModule({
   declarations: [
@@ -15,9 +20,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
+    UiModule,
+    HotToastModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es-ES'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

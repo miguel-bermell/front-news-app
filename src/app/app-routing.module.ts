be5dc81ed8b7b34a2135506507a9a556e6fbbef5
Miguel <miguelbermell@gmail.com>
2022-05-13 17:25:40 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
-import { PagesRoutingModule } from './core/pages/pages-routing.module'
 
 const routes: Routes = [
+  { path: '', loadChildren: async () => await import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'noticia/:id', loadChildren: async () => await import('./news/news.module').then(m => m.NewsModule) },
+  { path: 'archivo', loadChildren: async () => await import('./archived/archived.module').then(m => m.ArchivedModule) },
   { path: '**', redirectTo: '' }
 ]
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
-    PagesRoutingModule
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
